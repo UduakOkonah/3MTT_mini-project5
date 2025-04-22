4029b03e1e5f2948239ca8a0898584f0e68d7858
UduakOkonah <uduakobongokonah@gmail.com>
2025-04-22 18:47:19 +0100

    First Commit

---
+++ hello-world-react/src/App.jsx
@@ -0,0 +1,23 @@
+// src/App.jsx
+import './App.css'
+
+// --------------------------------------------
+//  Project Objective:
+// Build a simple React project that displays
+// "Hello World" using functional components,
+// semantic HTML, and modern styling. This app
+// serves as an introduction to React and JSX.
+// --------------------------------------------
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="card">
+        <h1>Hello World </h1>
+        <p>Welcome to my first React project built with Vite! #3MTT</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
